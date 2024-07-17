c4dea7cff7112218fb492103edf8af0528e58776
Saracier <mwojnowski@o2.pl>
2024-07-17 13:05:54 +0200

    feat: add flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    flash[:notice] = "aaa"
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const flashMessages = document.querySelectorAll('.flash');
+    flashMessages.forEach(function(flash) {
+        setTimeout(function() {
+            flash.style.display = 'none';
+        }, 3000);
+    });
+});
